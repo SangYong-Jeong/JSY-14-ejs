8fe8d1e63b857c3c805b67ffe0157dd8f98304fe
SangYong-Jeong <mmaf@naver.com>
2021-08-31 10:24:05 +0900

    1교시 완료

--- app.js
+++ app.js
@@ -1,54 +1,38 @@
 /************* global init ***************/
+const port = 3000
 const express = require('express')
 const app = express()
-const port = 3000
-const numeral = require('numeral')
-const _ = require('lodash')
 
-const coffee = [
-	{name:'아메리카노', price:2500},
-	{name:'카페라떼', price:3500},
-	{name:'바닐라라떼', price:3800},
-	{name:'카라멜 마끼아또', price:4500},
-]
-
-const food = [
-	{name:'제육덮밥', price:6500},
-	{name:'설렁탕', price:7500},
-	{name:'백반', price:5500},
-	{name:'돈까스', price:7500},
-]
-
-const desert = [
-	{name:'치즈케잌', price:5500},
-	{name:'딸기마카롱', price:4500},
-	{name:'바닐라쉐이크', price:5500},
-]
 
 /************ view engine ***************/
-app.set('view engine', 'ejs') 
+app.set('view engine', 'ejs')
 app.set('views', './views')
+
 
 /************* middleware ***************/
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }))
+
 
 /************* router init ***************/
 app.use('/', express.static('./public'))
-app.get('/coffee', (req, res, next) => {
-	
-	const sendMenus = _.cloneDeep(menus).map(v => {
-		v.price = numeral(v.price).format('0,0')
-		return v
-	})
-	// res.status(200).render(view파일, {})
-	res.status(200).render('menu', {menus: sendMenus})
+app.get('/about', (req, res, next) => {
+	const title = 'About Me'
+	const file = 'about'
+	const content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus iste, exercitationem veniam quis magni incidunt voluptates, dolor consectetur assumenda, enim necessitatibus amet reiciendis quae! Delectus, illum? Laborum nemo magnam ullam?'
+	res.render('about',{title, file, content})
 })
 
-app.get('/menu/:name', (req, res, next) => {
-	const name = req.params.name
-	res.status(200).render(name, {menu: eval(name), numeral})
+app.get('/pf', (req, res, next) => {
+	const title = 'Portfolio'
+	const file = 'pf'
+	const lists = [
+		{ title: 'PF_01', src: 'f1.jpg' },
+		{ title: 'PF_02', src: 'f2.jpg' },
+		{ title: 'PF_03', src: 'f3.jpg' },
+	]
 })
+
 
 /************* server function *************/
 app.listen(port, () => {console.log('http://127.0.0.1:'+port)})
